0188225cff2e222e202fa91c0dae8a841027893a
ANSHAM MAURYA <143842290+ANSHAM1@users.noreply.github.com>
2023-11-01 11:11:50 +0530

    Add files via upload

---
+++ sets_concept.py
@@ -0,0 +1,32 @@
+u={1,2,3,4}
+v={7,8,9}
+w={9,7,5,4,3,2}
+
+# u.add(2)
+
+# u.clear()
+
+# print(u.isdisjoint(v))
+
+# u.issubset(v)
+# u.issuperset(v)
+
+# u.pop() #---- remove the first entry in set 
+
+# print(u.symmetric_difference(v))
+# u.symmetric_difference_update(v)
+
+# u.update()
+
+
+
+# v.remove(7)
+# v.discard(3)
+
+# v.union(u)
+# v.intersection(w)
+
+# v.difference(w)
+
+
+print(u,v,w)
